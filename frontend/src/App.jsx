c32ff07efdc45de84c941616b3ed5acaf6904ad1
datonix.jsx <104119840+Datonix-art@users.noreply.github.com>
2024-04-16 22:11:18 +0400

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react'
+import ContactList from './ContactList';
+import ContactForm from './ContactForm';
+import './app.css'
+
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentContact, setCurrentContact] = useState()
+
+  useEffect(() => { // as soon as server loads it will call fetchContacts()
+    fetchContacts()
+  }, [])
+
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts")
+    const data = await response.json()
+    setContacts(data.contacts)
+  }
+  
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setCurrentContact({})
+  }
+
+  const openCreateModal = () => {
+    if(!isModalOpen) setIsModalOpen(true)
+  }
+   
+  const openEditModal = (contact) => {
+    if (isModalOpen) return
+    setCurrentContact(contact)
+    setIsModalOpen(true)
+  }
+  
+  const onUpdate = () => {
+    closeModal()
+    fetchContacts()
+  }
+
+  return (
+    <>
+      <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate}/>
+      <button onClick={openCreateModal}>Create new Contact</button>
+      { isModalOpen && <div className='modal'>
+       <div className='modal-content'>
+         <span className='close' onClick={closeModal}>&times;</span>
+         <ContactForm existingContact={currentContact}  updateCallback={onUpdate}/>
+       </div>
+      </div>
+      }
+    </>
+  )
+}
+
